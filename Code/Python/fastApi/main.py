57840090c414552350ce713363f79109e0f70c3a
bennnto <ben.promkaew@icloud.com>
2026-03-19 11:56:52 -0400

    Add CORS middle ware to render

--- Code/Python/fastApi/main.py
+++ Code/Python/fastApi/main.py
@@ -2,9 +2,19 @@ from typing import Annotated
 from datetime import datetime
 
 from fastapi import Body, FastAPI, Path, Query
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 
 app = FastAPI()
+
+# Enable CORS for all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 task_db = {}
 
